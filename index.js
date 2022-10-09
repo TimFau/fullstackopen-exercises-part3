45efd0afea8eb8f4cd53efa6a9a8bc63b8b5fbc8
Tim Fau <dev@timfau.com>
2022-10-08 21:54:17 -0400

    Completed Exercises -  3.1-3.6

---
+++ index.js
@@ -0,0 +1,84 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+let persons = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    console.log(id)
+    const person = persons.find(person => person.id === id)
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
+app.post('/api/persons', (request, response) => {
+    const name = request.body.name,
+          number = request.body.number,
+          id = Math.floor(Math.random() * 9999),
+          nameExists = persons.find(person => person.name === name),
+          handleError = (msg) => response.status(400).json({ error: msg })
+    if (!name) {
+        return handleError('Name Required')
+    }
+    if (!number) {
+        return handleError('Number Required')
+    }
+    if (nameExists) {
+        return handleError('Name must be unique')
+    }
+    const newPerson = {
+        id: id,
+        name: name,
+        number: number
+    }
+    persons = persons.concat(newPerson)
+    response.json(newPerson)
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+    response.status(204).end()
+})
+
+app.get('/info', (request, response) => {
+    response.send(`
+        <div>Phonebook has info for ${persons.length} people</div>
+        </br>
+        <div>${new Date}</div>
+    `)
+})
+
+const PORT = 3001
+app.listen(PORT)
+console.log(`Server is running on port ${PORT}`)
