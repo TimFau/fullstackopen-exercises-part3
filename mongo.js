ec8c7647bc81fbed841bce27e030d45789ceba6a
Tim Fau <dev@timfau.com>
2022-12-10 13:09:10 -0500

    Completed Exercise - 3.12 # Create a cloud-based MongoDB database for the phonebook application with MongoDB Atlas.

--- mongo.js
+++ mongo.js
@@ -5,47 +5,50 @@ if (process.argv.length < 3) {
   process.exit(1)
 }
 
-const password = process.argv[2]
+const password = process.argv[2],
+      name = process.argv[3],
+      number = process.argv[4]
 
-const noteContent = process.argv[3]
+const url = `mongodb+srv://timfau:${password}@cluster0.q0msdh2.mongodb.net/phonebookApp?retryWrites=true&w=majority`
 
-console.log(noteContent)
-
-const url = `mongodb+srv://timfau:${password}@cluster0.q0msdh2.mongodb.net/noteApp?retryWrites=true&w=majority`
-
-const noteSchema = new mongoose.Schema({
-  content: String,
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
   date: Date,
   important: Boolean,
 })
 
-const Note = mongoose.model('Note', noteSchema)
+const Person = mongoose.model('Person', personSchema)
 
 mongoose
   .connect(url)
   .then((result) => {
     console.log('connected')
 
-    // Add New Note
-
-    // const note = new Note({
-    //   content: noteContent,
-    //   date: new Date(),
-    //   important: true,
-    // })
-
-    // return note.save()
-
-    // Get All Notes
-    return Note.find({}).then(result => {
-      result.forEach(note => {
-        console.log(note)
+    if (name && number) {
+      // Add New Person
+      const person = new Person({
+        name: name,
+        number: number,
+        date: new Date(),
+        important: true,
       })
-      mongoose.connection.close()
+  
+      return person.save().then(() => {
+        console.log('added ' + name + ' number ' + number + ' to phonebook')
+      })
+    }
+
+    // Get All People
+    console.log('phonebook:')
+    return Person.find({}).then(result => {
+      result.forEach(person => {
+        console.log(person.name, person.number)
+      })
     })
   })
   .then(() => {
-    console.log('note saved!')
+    console.log('connection closed')
     return mongoose.connection.close()
   })
   .catch((err) => console.log(err))
